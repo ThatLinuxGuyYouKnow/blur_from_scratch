ea2043a2b442c832a6fb9554a23cbd71e6349cab
Emmanuels-code <arocket04@gmail.com>
2025-02-16 18:13:16 +0100

    chore: better file organization

---
+++ lib/UI/screens/homeSCreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeSCreen extends StatelessWidget {
+  HomeSCreen({super.key});
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
