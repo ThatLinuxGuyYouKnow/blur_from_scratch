3e6227b0273596e8c16f6ca27a230805dd0f3f23
Emmanuels-code <arocket04@gmail.com>
2025-02-16 20:48:27 +0100

    chore: refactored picker logic

--- lib/logic/pickImage.dart
+++ lib/logic/pickImage.dart
@@ -1,1 +1,18 @@
-pickImage() {}
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+
+pickImage() async {
+  FilePickerResult? result = await FilePicker.platform.pickFiles(
+    type: FileType.custom,
+    allowedExtensions: [
+      'jpg',
+      'pdf',
+    ],
+  );
+  if (result != null) {
+    File file = File(result.files.single.path!);
+    return file;
+  }
+  return null;
+}
